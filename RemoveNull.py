8021cec6bfaf7eb6d9fdc41dbb29fc1df349fc2c
Austin <47762009+acrumx@users.noreply.github.com>
2023-01-31 16:49:00 -0800

    Update RemoveNull.py
    First imports the dataset, checks how many null values exist in any of the columns, then removes nulls from the dataset, and lastly checks to see if all nulls have been removed. Then the data is exported as a new '.csv' file named 'FixedData.csv'

--- RemoveNull.py
+++ RemoveNull.py
@@ -4,7 +4,7 @@ pd.set_option('display.width', 600)
 pd.set_option('display.max_columns', 18)
 
 # Importing the dataset
-sales_df = pd.read_csv("C:/Users/acrum/Documents/train.csv")
+sales_df = pd.read_csv("C:/Users/Username/Documents/train.csv")
 print()
 
 # Finding if there are null values in any columns of the dataset
@@ -20,4 +20,4 @@ sales_df['Postal Code'] = sales_df['Postal Code'].fillna(5401)
 print(sales_df.isnull().sum())
 
 # Creating a new csv containing corrected data
-sales_df.to_csv("C:/Users/acrum/Documents/FixedData.csv")
+sales_df.to_csv("C:/Users/Username/Documents/FixedData.csv")
